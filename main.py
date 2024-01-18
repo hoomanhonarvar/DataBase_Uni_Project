7c17561bcfd391b5013b8bd9546c7ed62f85b816
pashmakhj <83176713+pashmakhj@users.noreply.github.com>
2024-01-19 02:56:56 +0330

    adding ER-diagram and commands of creating schemas in db

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("start")
+import os
+import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
+mydb=mysql.connector.connect(
+    host=os.environ['host'],
+    user=os.environ['user'],
+    password=os.environ['password'],
+    database=os.environ['database'],
+    auth_plugin='mysql_native_password'
+
+
+)
+# command="""
+# """
+#
+#
+# with mydb.cursor() as cursor:
+#     cursor.execute(command)
+#     mydb.commit()
+
